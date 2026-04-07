a8794c2b7a6a12fca99b5101f7c4af2d4eeb5f8c
Anrey-Baranov <dlyneero1@gmail.com>
2026-04-07 19:07:40 +0300

    Working

---
+++ BugPro/Program.cs
@@ -0,0 +1,133 @@
+﻿using Stateless;
+
+namespace BugPro;
+
+public class Bug
+{
+    //Состояния бага
+    public enum State
+    {
+        New,           //Новый дефект
+        Assigned,      //Назначен
+        InProgress,    //В работе
+        Fixed,         //Исправлен
+        ReadyForTest,  //Готов к тестированию
+        Closed,        //Закрыт
+        Reopened,      //Переоткрыт
+        Rejected,      //Отклонен
+        NeedInfo       //Требуется информация
+    }
+
+    public enum Trigger
+    {
+        Assign,        //Назначить
+        StartWork,     //Начать работу
+        Fix,           //Исправить
+        Test,          //Тестировать
+        Close,         //Закрыть
+        Reopen,        //Переоткрыть
+        Reject,        //Отклонить
+        NeedMoreInfo   //Запросить информацию
+    }
+
+    private readonly StateMachine<State, Trigger> _machine;
+    private readonly StateMachine<State, Trigger>.TriggerWithParameters<string> _assignTrigger;
+
+    public Bug(State initialState = State.New)
+    {
+        _machine = new StateMachine<State, Trigger>(initialState);
+        _assignTrigger = _machine.SetTriggerParameters<string>(Trigger.Assign);
+
+        ConfigureMachine();
+    }
+
+    private void ConfigureMachine()
+    {
+        _machine.Configure(State.New)
+            .Permit(Trigger.Assign, State.Assigned)
+            .Permit(Trigger.Reject, State.Rejected)
+            .Permit(Trigger.NeedMoreInfo, State.NeedInfo);
+
+        _machine.Configure(State.Assigned)
+            .Permit(Trigger.StartWork, State.InProgress)
+            .Permit(Trigger.Reject, State.Rejected);
+
+        _machine.Configure(State.InProgress)
+            .Permit(Trigger.Fix, State.Fixed)
+            .Permit(Trigger.Reject, State.Rejected);
+
+        _machine.Configure(State.Fixed)
+            .Permit(Trigger.Test, State.ReadyForTest);
+
+        _machine.Configure(State.ReadyForTest)
+            .Permit(Trigger.Close, State.Closed)
+            .Permit(Trigger.Reopen, State.Reopened)
+            .Permit(Trigger.NeedMoreInfo, State.NeedInfo);
+
+        _machine.Configure(State.Reopened)
+            .Permit(Trigger.Assign, State.Assigned)
+            .Permit(Trigger.Reject, State.Rejected);
+
+        _machine.Configure(State.Closed)
+            .Permit(Trigger.Reopen, State.Reopened);
+
+        _machine.Configure(State.Rejected)
+            .Permit(Trigger.Assign, State.Assigned)
+            .Permit(Trigger.Reopen, State.Reopened);
+
+        _machine.Configure(State.NeedInfo)
+            .Permit(Trigger.Assign, State.Assigned)
+            .Permit(Trigger.Reject, State.Rejected);
+    }
+
+    public State CurrentState => _machine.State;
+
+    public void Assign(string developer)
+    {
+        _machine.Fire(_assignTrigger, developer);
+    }
+
+    public void StartWork() => _machine.Fire(Trigger.StartWork);
+    public void Fix() => _machine.Fire(Trigger.Fix);
+    public void Test() => _machine.Fire(Trigger.Test);
+    public void Close() => _machine.Fire(Trigger.Close);
+    public void Reopen() => _machine.Fire(Trigger.Reopen);
+    public void Reject() => _machine.Fire(Trigger.Reject);
+    public void NeedMoreInfo() => _machine.Fire(Trigger.NeedMoreInfo);
+
+    public bool CanAssign => _machine.CanFire(Trigger.Assign);
+    public bool CanStartWork => _machine.CanFire(Trigger.StartWork);
+    public bool CanFix => _machine.CanFire(Trigger.Fix);
+    public bool CanTest => _machine.CanFire(Trigger.Test);
+    public bool CanClose => _machine.CanFire(Trigger.Close);
+    public bool CanReopen => _machine.CanFire(Trigger.Reopen);
+    public bool CanReject => _machine.CanFire(Trigger.Reject);
+    public bool CanNeedMoreInfo => _machine.CanFire(Trigger.NeedMoreInfo);
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var bug = new Bug();
+        Console.WriteLine($"Начальное состояние: {bug.CurrentState}");
+
+        bug.Assign("Иван Иванов");
+        Console.WriteLine($"После назначения: {bug.CurrentState}");
+
+        bug.StartWork();
+        Console.WriteLine($"После начала работы: {bug.CurrentState}");
+
+        bug.Fix();
+        Console.WriteLine($"После исправления: {bug.CurrentState}");
+
+        bug.Test();
+        Console.WriteLine($"После тестирования: {bug.CurrentState}");
+
+        bug.Close();
+        Console.WriteLine($"После закрытия: {bug.CurrentState}");
+
+        bug.Reopen();
+        Console.WriteLine($"После переоткрытия: {bug.CurrentState}");
+    }
+}
---
+++ BugTests/Test1.cs
@@ -0,0 +1,282 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BugPro;
+using Stateless;
+using System;
+
+namespace BugTests;
+
+[TestClass]
+public class BugTests
+{
+    [TestMethod]
+    public void Constructor_DefaultState_ShouldBeNew()
+    {
+        var bug = new Bug();
+
+        Assert.AreEqual(Bug.State.New, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Constructor_CustomInitialState_ShouldBeSpecifiedState()
+    {
+        var bug = new Bug(Bug.State.Reopened);
+
+        Assert.AreEqual(Bug.State.Reopened, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Assign_FromNew_ShouldChangeToAssigned()
+    {
+        var bug = new Bug();
+
+        bug.Assign("Developer");
+
+        Assert.AreEqual(Bug.State.Assigned, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void StartWork_FromAssigned_ShouldChangeToInProgress()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+
+        bug.StartWork();
+
+        Assert.AreEqual(Bug.State.InProgress, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Fix_FromInProgress_ShouldChangeToFixed()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+
+        bug.Fix();
+
+        Assert.AreEqual(Bug.State.Fixed, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Test_FromFixed_ShouldChangeToReadyForTest()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+
+        bug.Test();
+
+        Assert.AreEqual(Bug.State.ReadyForTest, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Close_FromReadyForTest_ShouldChangeToClosed()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+        bug.Test();
+
+        bug.Close();
+
+        Assert.AreEqual(Bug.State.Closed, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Reopen_FromClosed_ShouldChangeToReopened()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+        bug.Test();
+        bug.Close();
+
+        bug.Reopen();
+
+        Assert.AreEqual(Bug.State.Reopened, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Reject_FromNew_ShouldChangeToRejected()
+    {
+        var bug = new Bug();
+
+        bug.Reject();
+
+        Assert.AreEqual(Bug.State.Rejected, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void NeedMoreInfo_FromNew_ShouldChangeToNeedInfo()
+    {
+        var bug = new Bug();
+
+        bug.NeedMoreInfo();
+
+        Assert.AreEqual(Bug.State.NeedInfo, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Assign_FromRejected_ShouldChangeToAssigned()
+    {
+        var bug = new Bug();
+        bug.Reject();
+
+        bug.Assign("Developer");
+
+        Assert.AreEqual(Bug.State.Assigned, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void Assign_FromNeedInfo_ShouldChangeToAssigned()
+    {
+        var bug = new Bug();
+        bug.NeedMoreInfo();
+
+        bug.Assign("Developer");
+
+        Assert.AreEqual(Bug.State.Assigned, bug.CurrentState);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void Close_FromNew_ShouldThrowException()
+    {
+        var bug = new Bug();
+
+        bug.Close();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void Fix_FromNew_ShouldThrowException()
+    {
+        var bug = new Bug();
+
+        bug.Fix();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void Test_FromNew_ShouldThrowException()
+    {
+        var bug = new Bug();
+
+        bug.Test();
+    }
+
+    [TestMethod]
+    public void CanAssign_FromNew_ShouldReturnTrue()
+    {
+        var bug = new Bug();
+
+        Assert.IsTrue(bug.CanAssign);
+    }
+
+    [TestMethod]
+    public void CanAssign_FromInProgress_ShouldReturnFalse()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+
+        Assert.IsFalse(bug.CanAssign);
+    }
+
+    [TestMethod]
+    public void CanClose_FromReadyForTest_ShouldReturnTrue()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+        bug.Test();
+
+        Assert.IsTrue(bug.CanClose);
+    }
+
+    [TestMethod]
+    public void CanClose_FromNew_ShouldReturnFalse()
+    {
+        var bug = new Bug();
+
+        Assert.IsFalse(bug.CanClose);
+    }
+
+    [TestMethod]
+    public void CanReopen_FromClosed_ShouldReturnTrue()
+    {
+
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+        bug.Test();
+        bug.Close();
+
+        Assert.IsTrue(bug.CanReopen);
+    }
+
+    [TestMethod]
+    public void FullWorkflow_ShouldCompleteSuccessfully()
+    {
+        var bug = new Bug();
+
+        bug.Assign("Developer");
+        Assert.AreEqual(Bug.State.Assigned, bug.CurrentState);
+
+        bug.StartWork();
+        Assert.AreEqual(Bug.State.InProgress, bug.CurrentState);
+
+        bug.Fix();
+        Assert.AreEqual(Bug.State.Fixed, bug.CurrentState);
+
+        bug.Test();
+        Assert.AreEqual(Bug.State.ReadyForTest, bug.CurrentState);
+
+        bug.Close();
+        Assert.AreEqual(Bug.State.Closed, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void ReopenAndAssign_ShouldWorkCorrectly()
+    {
+        var bug = new Bug();
+        bug.Assign("Dev1");
+        bug.StartWork();
+        bug.Fix();
+        bug.Test();
+        bug.Close();
+
+        bug.Reopen();
+        bug.Assign("Dev2");
+        bug.StartWork();
+
+        Assert.AreEqual(Bug.State.InProgress, bug.CurrentState);
+    }
+
+    [TestMethod]
+    public void MultipleReopens_ShouldBePossible()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+        bug.Test();
+        bug.Close();
+
+        bug.Reopen();
+        Assert.AreEqual(Bug.State.Reopened, bug.CurrentState);
+
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+        bug.Test();
+        bug.Close();
+
+        Assert.AreEqual(Bug.State.Closed, bug.CurrentState);
+    }
+}
